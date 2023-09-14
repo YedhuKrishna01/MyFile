dbe60d4fed25c1b2e6b8c5ebb46dfa8f403bc114
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-09-14 13:28:56 +0530

    Add files via upload

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,24 @@
-let a=0;
-do{
-    
-    console.log(a);
-    a++;
+let arr=[1,4,6,9,3];
+console.log(arr[3])
+let b={
+    name:"john",
+    address:[2,5,7,7,0],
+    names:{
+        place:"paris",
+        fruit:"apple",
+        color:"red"
+    }
 }
-while(a<5)
+console.log(b.address[2]);
+console.log(b.names.fruit);
+arr.push(5,6,7);
+console.log(arr);
+function funt1(i){
+    arr.push(i,"john");
+    console.log(arr);
+}
+funt1(3);
+console.log(arr.includes(0));
+console.log(arr.at(4));
+console.log(arr.indexOf("john"));
+

768c82e4a5ab3d95dadc7a5fd3df2bb0e28fdb94
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-10-03 13:20:44 +0530

    Add files via upload

--- Javascript/latestwork/script.js
+++ Javascript/latestwork/script.js
@@ -11,6 +11,5 @@ let prod = document.getElementById("pCard");
             <img src="${element.thumbnail}">
             <a href = "http://127.0.0.1:5500/prod.html?id=${element.id}"><br><input type="submit" style="background-color:red;color:white;margin-top:5vh;border:none;font-size:25px;border-radius:10px;padding:1vh 2vh" value="view"></a>`
             prod.innerHTML+=card;
-        
             });
         })

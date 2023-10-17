d0f724002932354c31882ce22c82dc1b1abf7d39
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-10-17 13:14:02 +0530

    Add files via upload

--- Javascript/new/node.js
+++ Javascript/new/node.js
@@ -1,18 +1,28 @@
-// let a = 21;
-// console.log(a);
 const http = require("http");
+const url = require("url");
 const port = 3000;
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    let url = String(req.headers.referer);
-    let api = url.split("3000/")[1];
-    console.log(api);
-    let form = `<form action="http://localhost:3000"><input type="text"><br><input type="submit"></form>`;
-    res.write(form);
-    res.end();
-
+    let path = url.parse(req.url).pathname;
+    if(path ==="/"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write(`
+        <form action="/api" method ="get">
+        <h3>Enter your Username:</h3>
+        <input type="text" class="username" name = "username">
+        <h3>Enter your Emailid:</h3>
+        <input type="email" name = "email">
+        <input type = "submit">
+        </form>`);
+        res.end();
+    }
+    if(path==="/api"){
+        console.log(url.parse(req.url,true).query);
+        res.write(`Username is ${ /^[a-z0-9]{4,}$/.test(query.username)? "Valid":"Invalid"});
+        Email is ${ /^[a-z0-9]+@[a-z0-9]+\.[a-z]{2,6}$/.test(query.email)? "Valid":"Invalid"}`);
+        res.end();
+    }
 });
-server.listen(port,(error)=>{
+server.listen(port, error =>{
     if(error){
         console.log(error);
     }
@@ -20,3 +30,4 @@ server.listen(port,(error)=>{
         console.log("server started on port " + port);
     }
 });
+

9519b00bd761618ac6f8d3ba135577d7a279643d
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-10-19 13:31:46 +0530

    Add files via upload

--- Javascript/new2/node.js
+++ Javascript/new2/node.js
@@ -5,35 +5,53 @@ const port = 2000;
 const server = http.createServer((req, res) => {
   let path = url.parse(req.url).pathname;
   if (path === "/") {
-    fs.readFile("./index.html","utf-8",(error,data)=>{
-        console.log(data);
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
+    fs.readFile("./index.html", "utf-8", (error, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
     })
-    
+
   }
-  if (path === "/api") {
+  if (path === "/set-todo") {
     let query = url.parse(req.url, true).query;
-    fs.readFile("./data.json","utf-8",(error,data)=>{
-        let list = data ===""? []:JSON.parse(data);
-        list.push(query);
-        fs.writeFile("./data.json",JSON.stringify(list),(error) =>{
-            if(error){
-                res.write("Error has occured");
-            }
-        })
+    console.log(query);
+    fs.readFile("./data.json", "utf-8", (error, data) => {
+      let list = data === "" ? [] : JSON.parse(data);
+      list.push(query);
+      fs.writeFile("./data.json", JSON.stringify(list), (error) => {
+        if (error) {
+          res.write("Error has occured");
+        }
+      })
 
     })
-    res.write(`Todo list ${/^[a-z0-9]{4,}$/.test(query.toDoList) ? "Modified" : "Remains the same"}`);
     res.end();
   }
-});
+  if (path === "/get-todo") {
+    fs.readFile("./data.json", "utf-8", (error, data) => {
+      console.log(data);
+      res.writeHead(200, { "Content-Type": "text/json" });
+      res.write(JSON.stringify(data));
+      res.end()
 
-server.listen(port, (error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("server started on port " + port);
+    })
   }
-});
+  if (path === "/del-todo") {
+    let query = url.parse(req.url, true).query;
+    fs.readFile("./data.json", "utf-8", (error, data) => {
+      let dataObj = JSON.parse(data);
+      fs.writeFile("./data.json", JSON.stringify(dataObj.filter(element => element.todo !== query.todo)), error => {
+        res.write("delete");
+        if (error) {
+          res.write("Error occured");
+        }
+        res.end()
+
+
+        server.listen(port, error => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("server started on port " + port);
+          }
+        })

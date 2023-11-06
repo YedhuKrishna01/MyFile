65d0e64e9b7cb0475634aa488e4f486bce608b19
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-11-06 11:13:56 +0530

    Update request-handler.js

--- dataToDatabase/request-handler.js
+++ dataToDatabase/request-handler.js
@@ -1,90 +1,114 @@
-import UserSchema from "./schemas/user.schema.js";
+import UserSchema from "./schemas/userschemas.js";
 import loginSchema from "./schemas/login.schema.js";
-export async function setData(req,res){
+import bcrypt from "bcrypt";
+
+export async function setData(req ,res) {
     try{
-        let {id,firstname,lastname,email,password}=req.body;
-        console.log(req.body);
-        let result = await UserSchema.create({id,firstname,lastname,email,password})
-        res.status(200).send(result);
-    }catch(error){
-        console.log(error);
-        res.json("ERROR")
+         let {id,username,firstname,lastname,email,phone,password} =req.body;
+         let result= await UserSchema.create({id,username,firstname,lastname,email,phone,password});
+         console.log(req.body);
+
+         res.status(200).send(result);
+    }catch  (error) {
+      console.log(error);
+      res.json("ERROR")
     }
 }
 export async function getData(req,res){
-    try{
-        let { id } = req.query;
-        console.log(req.query)
-        let data=await UserSchema.findOne({id:id});
-        res.json(data);
-    }catch(error){
-        console.log(error);
-        res.status(500).send("Some error");
-    }
+try {
+  let{id}= req.query;
+     let data = await UserSchema.find();
+     res.json(data);
+} catch (error) {
+  console.log(error);
+  res.status(500).send("some error");
 }
-export async function updateData(req,res){
-    try{
-        let {id}=req.query;
-        let data =req.body;
-        let result = await UserSchema.updateOne({_id:id},data);
-        res.json(result);
-    }catch(error){
-        console.log(error);
-        res.status(500).send("Some error");
-    }
+
 }
-export async function deleteData(req,res){
-    try{
-        let {id}=req.query;
-        let result = await UserSchema.deleteOne({_id:id});
-        res.json(result);
-    }catch(error){
-        console.log(error);
-        res.json("Some error");
-    }
+
+
+export async function getData2(req,res){
+  try {
+       let{id}= req.query;
+       let data = await UserSchema.findOne({id:id});
+       res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("some error");
+  }
+  
+  }
+export async function updateData(req,res) {
+  try {
+    let {id} = req.query;
+    let data = req.body;
+    let result = await UserSchema.updateOne({_id: id},data);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.json("some error occured");
+    
+  }
 }
-export function users(req,res) {
-    try {
-       let id =req.params;
+export async function deleteData(req,res) {
+  try {
+    let {id} =req.query;
+    let result=await UserSchema.deleteOne({_id: id})
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.json("some errors occured")
+    
+  }
+}
+
+
+export function users(req ,res) {
+  try{
+       let id=req.params
        console.log(id);
-       console.log(req.query);
-       res.status(200).send("userdata")
-    } catch (error) {
-        console.log(error);
-        res.json("Error")
-    }
+       console.log(req.query)
+       res.status(200).send("data")
+  }catch  (error) {
+    console.log(error);
+    res.json("ERROR")
+  }
 }
-
 export async function register(req,res){
-    try {
-        let { username,password } = req.body;
-        if(password.length <4){
-            return res.json("Invalid pass");
-        }
-        let user = await loginSchema.findOne({username})
-        let hashedPass=await bcrypt.has(password,10)
-        if(user){
-            return res.json("user already exists");
-        }
-        let result = await loginSchema.create({username,password:hashedPass});
-        return res.json(result);
-    } catch (error) {
-        console.log(error);
-        res.json("error occured");
-    }
-}
 
-export async function login(req,res){
-    try {
-        let { username,password } = req.body;
-        let user = await loginSchema.findOne({ username });
-        let validation = await bcrypt.compare(password,user.password);
-        if(validation){
-            return res.json("login successfull");
-        }
-        return res.json("incorrect username or password");
-    } catch (error) {
-        console.log(error);
-        res.json("error occured")
+  try {
+    let {username , password} = req.body;
+    if(password.length<4) {
+    return res.json("invalid pass");
+       
     }
+    let user = await loginSchema.findOne({username})
+    let hashedpass = await bcrypt.hash(password,10)
+    if (user) {
+      return res.json("user already exists");
+    }
+    let result = await loginSchema.create({ username,password:hashedpass});
+  } catch (error) {
+    console.log(error);
+    res.json("Error occured")
+  }
 }
+export async function login(req,res){
+  try {
+    let {username,password } =req.body;
+    let user= await loginSchema.findOne({username});
+    let validation = await bcrypt.compare(password,user.password);
+    if(validation) {
+        return res.json("Login successful");
+    }
+  
+  
+  return res.json("Incorrect username or password");
+
+  } catch (error) {
+    console.log(error);
+    res.json("Error occured")
+    
+  }
+
+      }

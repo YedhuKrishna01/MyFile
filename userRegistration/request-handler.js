27f1c3a292109a0eb81fba5e23dd2bdeb4bd9976
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-11-08 13:25:32 +0530

    Add files via upload

--- userRegistration/request-handler.js
+++ userRegistration/request-handler.js
@@ -1,5 +1,9 @@
 import bcrypt from "bcrypt";
 import userSchema from "./model/user.schema.js";
+import jwt from "jsonwebtoken"
+
+const {sign}=jwt;
+
 export async function register(req,res){
     try {
         let { username, password } = req.body;
@@ -15,6 +19,61 @@ export async function register(req,res){
         return res.status(200).send("Successfull");
     } catch (error) {
         console.log(error);
-        res.status(200).send("Error");
+        res.status(500).send("Error");
+    }
+}
+
+export async function login(req,res){
+    try {
+        let { username, password } = req.body;
+        if( username.length<4 && password.length<4 ){
+            return res.json("Invalid")
+        }
+        let user = await userSchema.findOne({ username });
+        if(!user){
+            return res.status(403).send("Invalid");
+        }
+        let passCheck = await bcrypt.compare(password,user.password);
+        if(passCheck){
+            let token = await sign({
+                username:user.username,
+                id:user._id
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn:"24h"
+            })
+            return res.status(200).json({
+                msg:"Login successful..",
+                token:token
+            })
+        }
+        return res.status(403).send("invalid")
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+}
+
+export async function profile(req, res){
+    try {
+        let user = req.user;
+        let userDetails = await userSchema.findOne({_id:user.id},{password:0});
+        if(userDetails){
+            return res.json(userDetails);
+        }
+        return res.status(404).send("user not found");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+}
+
+export async function addNote(req, res){
+    try {
+        let { note } = req.body;
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
     }
 }
--- userRegistration/router.js
+++ userRegistration/router.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import * as userHandler from "./request-handler.js";
 const router = Router()
-router.route("/register").post(userHandler.register)
+router.route("/register").post(userHandler.register);
+router.route("/login").post(userHandler.login);
+router.route("/profile").get(userHandler.profile);
+router.route("/login").post(userHandler.login);
 export default router;

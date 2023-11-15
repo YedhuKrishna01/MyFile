0ba699cf2228282714ed7508b06a7261aeef6ebd
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-11-15 13:02:35 +0530

    Delete bookMyShow directory

--- bookMyShow/connection.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export default function connect(){
-    return mongoose.connect(process.env.DB_URI);
-}
--- bookMyShow/router.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import * as rh from "./request-handler.js";
-const router = Router();
-router.route("/add-review").post(rh.add);
-
-export default router;

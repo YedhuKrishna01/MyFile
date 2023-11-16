2e1fddb84f7959af8babe33c998fce12478c9d1d
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-11-16 11:16:36 +0530

    Add files via upload

--- bookMyShow/router.js
+++ bookMyShow/router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as rh from "./request-handler.js";
 const router = Router();
-router.route("/add-review").post(rh.add);
+router.route("/add").post(rh.add);
 
 export default router;
--- bookMyShow/server.js
+++ bookMyShow/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import router from "./router.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import conn from "./connection.js"
+import conn from "./connection.js";
+import path from "path";
+
 dotenv.config();
 const server = express();
 
@@ -12,16 +14,36 @@ server.use(morgan("tiny"));
 server.use("/", express.static("./static"));
 server.use('/images', express.static('images'));
 
+// Store movie data
+let movies = [];
+
 server.use("/api", router);
+
+// Endpoint to add a movie
+server.post("/api/addMovie", (req, res) => {
+    const { movieName, posterURL } = req.body;
+
+    if (movieName && posterURL) {
+        movies.push({ movieName, posterURL });
+        res.status(200).json({ success: true, message: "Movie added successfully." });
+    } else {
+        res.status(400).json({ success: false, message: "Invalid movie data." });
+    }
+});
+
+// Endpoint to get all movies
+server.get("/api/getMovies", (req, res) => {
+    res.status(200).json({ success: true, movies });
+});
+
 conn().then(() => {
-   server.listen(process.env.PORT, (error) => {
-      if(error) {
-         console.log(error);
-         return;
-      }
-      console.log("Server started on port 3000");
-   })
-})
-.catch((error)=>{
-   console.log(error)
-})
+    server.listen(process.env.PORT, (error) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        console.log("Server started on port 3000");
+    });
+}).catch((error) => {
+    console.log(error);
+});

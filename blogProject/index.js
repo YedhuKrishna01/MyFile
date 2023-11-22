0471b5b79495a0eed229586bc8b37aa302d54a8d
YedhuKrishna01 <142202698+YedhuKrishna01@users.noreply.github.com>
2023-11-22 13:19:14 +0530

    Add files via upload

--- blogProject/index.js
+++ blogProject/index.js
@@ -3,13 +3,16 @@ import { dirname } from "path";
 import { fileURLToPath} from "url";
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
-const port = 3200;
+const port = 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 app.get("/",(req, res)=>{
     res.sendFile(_dirname+'/public/html/index.html')
+})
+app.get("/register",(req, res)=>{
+    res.sendFile(_dirname+'/public/html/register.html')
 })
 
 app.listen(port,()=>{
